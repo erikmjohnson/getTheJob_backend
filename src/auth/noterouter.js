708cb1b8619a56c850cf9cb89cbfa8db1418c426
erikmjohnson <erikarisma98@gmail.com>
2019-07-17 19:15:52 -0700

    working on deleting item from database

--- src/auth/noterouter.js
+++ src/auth/noterouter.js
@@ -8,6 +8,35 @@ const noteRouter = express.Router();
 const Notes = require('../userDBSchema/noteSchema');
 const User = require('../userDBSchema/users-model');
 
+/** retrieves data for a username*/
+noteRouter.get('/retrieve/:username', (req, res, next) => {
+  User.find({username:`${req.params.username}`})
+    .populate('notes')
+    //.exec acts like a promise.
+    .then(data => {
+      res.status(200).json(data[0].notes);
+      return data[0].notes;
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: err});
+    });
+});
+
+noteRouter.delete('/delete/:username', (req, res, next) => {
+  User.find({username:`${req.params.username}`})
+    .populate('notes')
+    .then(data => {
+      return data[0].notes.filter(job => job._id !== req.body._id);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: err});
+    })
+});
+
+
+
 /** saves the note to the job for later reference*/
 noteRouter.post('/save/:username', (req, res, next) => {
   let note = new Notes(req.body);
@@ -20,21 +49,10 @@ noteRouter.post('/save/:username', (req, res, next) => {
       user[0].notes.push(note);
       user[0].save();
     })
-});
-
-noteRouter.get('/retrieve/:username', (req, res, next) => {
-  User.find({username:`${req.params.username}`})
-    .populate('notes')
-    //.exec acts like a promise.
-    .then(data => {
-      res.status(200).json(data[0].notes);
-      return data[0].notes;
-      })
     .catch(err => {
       console.log(err);
       res.status(500).json({error: err});
     });
-
 });
 
 /** Exports noteRouter for use outside of this file.*/
